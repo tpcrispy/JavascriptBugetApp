bf8ab23795f53a5d07e9236571a665472e163f51
tpcrispy <thomas.1993.smith@gmail.com>
2017-02-07 10:14:37 +1100

    setup Constructors for inc/exp

--- app.js
+++ app.js
@@ -1,7 +1,31 @@
 // BUDGET CONTROLLER
 const budgetController = (function(){
 
-})();
+
+  const Expense = function(id, description, value) {
+    this.id: id;
+    this.description: description;
+    this.value: value;
+  };
+
+  const Income = function(id, description, value) {
+    this.id: id;
+    this.description: description;
+    this.value: value;
+  };
+
+  const data = {
+    allItems: {
+      exp: [],
+      inc: []
+    },
+    totals: {
+      exp: 0,
+      inc: 0
+    }
+  };
+
+  })();
 
 
 
@@ -42,7 +66,6 @@ const controller = (function (budgetCtrl, UICtrl){
     });
   };
 
-
   const ctrlAddItem = function(){
     //values of field input
     const input = UICtrl.getinput();
@@ -55,8 +78,6 @@ const controller = (function (budgetCtrl, UICtrl){
       setupEventListeners();
     }
   }
-
-
 })(budgetController, UIController);
 
 controller.init();
