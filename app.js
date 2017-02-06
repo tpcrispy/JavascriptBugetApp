a0d7719bfce47bcbf3a0cee9a9950eefa1d28caa
tpcrispy <thomas.1993.smith@gmail.com>
2017-02-07 09:40:56 +1100

    setting up controllers + getting inputs of textfeilds

--- app.js
+++ app.js
@@ -1,13 +1,85 @@
-var budgetController = (function(){
-  var x = 23;
+// BUDGET CONTROLLER
+const budgetController = (function(){
 
- var add = function (a){
-    return x + a;
+})();
+
+
+
+// UI CONTROLLER
+const UIController = (function (){
+
+  const DOMstrings = {
+    inputType: '.add__type',
+    inputDescription: '.add__description',
+    inputValue: '.add__value',
+    inputBtn: '.add__btn'
   }
 
   return {
-    publicTest: function(b) {
-      console.log(add(b));
+    getinput: function(){
+      return {
+        type: document.querySelector(DOMstrings.inputType).value, //Will be either inc or expenses
+        description: document.querySelector(DOMstrings.inputDescription).value,
+        value: document.querySelector(DOMstrings.inputValue).value
+      }
+    },
+    getDOMstrings: function() {
+      return DOMstrings;
     }
-  }
+  };
 })();
+
+
+const controller = (function (budgetCtrl, UICtrl){
+  const DOM = UICtrl.getDOMstrings();
+
+  const ctrlAddItem = function(){
+    //values of field input
+    const input = UICtrl.getinput();
+    console.log(input);
+
+
+  }
+  document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
+  document.addEventListener('keypress', function(e){
+    //if the enter key is pressed
+    if (e.keyCode === 13 || e.which === 13) {
+      ctrlAddItem();
+    }
+  });
+
+})(budgetController, UIController);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
