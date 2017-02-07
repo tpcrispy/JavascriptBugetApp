5c14bbc5d210277ddb455663569e573e03969bc5
tpcrispy <thomas.1993.smith@gmail.com>
2017-02-07 13:43:55 +1100

    wired up displaying the income / expenses inside the DOM

--- app.js
+++ app.js
@@ -61,7 +61,9 @@ const UIController = (function (){
     inputType: '.add__type',
     inputDescription: '.add__description',
     inputValue: '.add__value',
-    inputBtn: '.add__btn'
+    inputBtn: '.add__btn',
+    incomeContainer: '.income__list',
+    expensesContainer: '.expenses__list'
   }
 
   return {
@@ -71,6 +73,21 @@ const UIController = (function (){
         description: document.querySelector(DOMstrings.inputDescription).value,
         value: document.querySelector(DOMstrings.inputValue).value
       }
+    },
+    addListItem: function(obj, type){
+      var html, newHtml, element;
+      if(type=='exp') {
+        element = DOMstrings.expensesContainer;
+        html = '<div class="item clearfix" id="expense-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">-%value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>'
+      } else {
+        element = DOMstrings.incomeContainer;
+        html = '<div class="item clearfix" id="income-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">+%value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>'
+      }
+        newHtml = html.replace('%id%', obj.id);
+        newHtml = newHtml.replace('%description%', obj.description);
+        newHtml = newHtml.replace('%value%', obj.value);
+
+        document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
     },
     getDOMstrings: function() {
       return DOMstrings;
@@ -99,6 +116,10 @@ const controller = (function (budgetCtrl, UICtrl){
 
     // add the item to the budget controller
      newItem = budgetCtrl.addItem(input.type, input.description, input.value);
+
+     UICtrl.addListItem(newItem, input.type);
+
+
   };
 
   return {
